bd5cf169af84a1d023090b82dbe0928b5616d880
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2018-07-13 16:17:23 +0100

    crawlies above and behind the crawlie

--- src/test/scala/life/LifeSpec.scala
+++ src/test/scala/life/LifeSpec.scala
@@ -7,40 +7,66 @@ class LifeSpec extends WordSpec with MustMatchers {
   "howManyAliveNeighbours" should {
     "return 0 when there are no alive neighbours in a sequence" in {
 
-      val crawlie1 = Crawlie(1,1,true)
+      val testCrawlie = Crawlie(1,1,true)
       val crawlie2 = Crawlie(1,2,false)
       val crawlie3 = Crawlie(2,1,false)
       val crawlie4 = Crawlie(2,2,false)
 
-      val crawlies = Seq(crawlie1, crawlie2, crawlie3, crawlie4)
+      val crawlies = Seq(testCrawlie, crawlie2, crawlie3, crawlie4)
 
-      crawlie1.howManyAliveNeighbours(crawlies) mustBe 0
+      testCrawlie.howManyAliveNeighbours(crawlies) mustBe 0
 
     }
 
     "return 1 when there is 1 alive neighbour in a sequence" in {
 
-      val crawlie1 = Crawlie(1,1,true)
+      val testCrawlie = Crawlie(1,1,true)
       val crawlie2 = Crawlie(1,2,true)
       val crawlie3 = Crawlie(2,1,false)
       val crawlie4 = Crawlie(2,2,false)
 
-      val crawlies = Seq(crawlie1, crawlie2, crawlie3, crawlie4)
+      val crawlies = Seq(testCrawlie, crawlie2, crawlie3, crawlie4)
 
-      crawlie1.howManyAliveNeighbours(crawlies) mustBe 1
+      testCrawlie.howManyAliveNeighbours(crawlies) mustBe 1
 
     }
 
     "return 2 when there are 2 alive neighbours in a sequence" in {
 
-      val crawlie1 = Crawlie(1,1,true)
+      val testCrawlie = Crawlie(1,1,true)
       val crawlie2 = Crawlie(1,2,true)
       val crawlie3 = Crawlie(2,1,true)
       val crawlie4 = Crawlie(2,2,false)
 
-      val crawlies = Seq(crawlie1, crawlie2, crawlie3, crawlie4)
+      val crawlies = Seq(testCrawlie, crawlie2, crawlie3, crawlie4)
 
-      crawlie1.howManyAliveNeighbours(crawlies) mustBe 2
+      testCrawlie.howManyAliveNeighbours(crawlies) mustBe 2
+
+    }
+
+    "return 1 when there is 1 alive neighbour in a sequence behind the crawlie" in {
+
+      val crawlie1 = Crawlie(1,1,true)
+      val testCrawlie = Crawlie(1,2,true)
+      val crawlie3 = Crawlie(2,1,false)
+      val crawlie4 = Crawlie(2,2,false)
+
+      val crawlies = Seq(crawlie1, testCrawlie, crawlie3, crawlie4)
+
+      testCrawlie.howManyAliveNeighbours(crawlies) mustBe 1
+
+    }
+
+    "return 1 when there is 1 alive neighbour in a sequence above the crawlie" in {
+
+      val crawlie1 = Crawlie(1,1,true)
+      val crawlie2 = Crawlie(1,2,false)
+      val testCrawlie = Crawlie(2,1,true)
+      val crawlie4 = Crawlie(2,2,false)
+
+      val crawlies = Seq(crawlie1, crawlie2, testCrawlie, crawlie4)
+
+      testCrawlie.howManyAliveNeighbours(crawlies) mustBe 1
 
     }
   }
